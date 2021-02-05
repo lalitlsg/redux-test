e2abc269147df43c291366c3aed9285fc2a64691
Lalit Garghate <lalit.garghate@gmail.com>
2021-02-05 19:10:23 +0530

    hooked redux into react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter = {counter}</h1>
+      {isLogged && <p>User LoggedIn</p>}
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
